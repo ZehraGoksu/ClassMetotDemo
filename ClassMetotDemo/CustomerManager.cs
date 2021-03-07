190a98d8929b7d0ae693dde634e23c6fb587e8df
Zehra GÖKSU <edaxzehra@hotmail.com>
2021-03-07 12:27:56 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+	class CustomerManager
+	{
+			List<Customer> customers = new List<Customer>();
+
+			public void List(Customer customer)
+			{
+				foreach (var item in customers)
+				{
+					Console.WriteLine(item.Id + " " + item.Name + " " + item.Surname + "  " + item.Age + " " + item.Email);
+				}
+			}
+			public void Add(Customer customer)
+			{
+				Console.WriteLine(customer.Name + " " + "adlı müşteri başarılı bir şekilde kaydedildi.");
+
+			}
+
+			public void Delete(Customer customer)
+			{
+				Console.WriteLine(customer.Name + " " + "adlı müşteri başarılı bir şekilde silindi.");
+
+			}
+
+		
+	 }
+	
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+				Customer customer1 = new Customer { Id = 1, Name = "Fuat", Surname = "Kara", Age = 25, Email = "123@gmail.com" };
+				Customer customer2 = new Customer { Id = 2, Name = "Oktay", Surname = "Güzel", Age = 56, Email = "wygrg@gmail.com" };
+				Customer customer3 = new Customer { Id = 3, Name = "Funda", Surname = "Hane", Age = 73, Email = "fhbf@gmail.com" };
+				Customer customer4 = new Customer { Id = 4, Name = "Sanem", Surname = "Varlı", Age = 34, Email = "fbhf@gmail.com" };
+				Customer customer5 = new Customer { Id = 5, Name = "Tilbe", Surname = "Kendi", Age = 39, Email = "xsfb@gmail.com" };
+
+				CustomerManager manager = new CustomerManager();
+				manager.Add(customer3);
+				manager.Delete(customer5);
+
+				Customer[] customers = new Customer[] { customer1, customer2, customer3, customer4, customer5 };
+				foreach (var item in customers)
+				{
+					Console.WriteLine(item.Id + "." + item.Name + " " + item.Surname + "  Yaş: " + item.Age + "  E-Posta " + item.Email);
+
+				}
+
+			
+		}
+	}
+	
+}
+	
